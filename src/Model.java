96618c076f3e52b7560a9ca5ff9f881cf02368ee
Trong Nhan <imouto1994@gmail.com>
2013-09-18 23:26:31 +0800

    Edit and format these 2 classes

--- src/Model.java
+++ src/Model.java
@@ -1,46 +1,90 @@
 import java.util.ArrayList;
 
-class Model{
+class Model {
 	private ArrayList<Task> pending;
 	private ArrayList<Task> complete;
 	private ArrayList<Task> trash;
 	private ArrayList<Task> search;
+
 	// constructor
-	public Model(){
+	public Model() {
 		pending = new ArrayList<Task>();
 		complete = new ArrayList<Task>();
 		trash = new ArrayList<Task>();
 		search = new ArrayList<Task>();
 	}
-	
+
 	// get functions
-	public Task getTaskFromPending(int index){ return pending.get(index); }
-	public Task getTaskFromComplete(int index){ return complete.get(index); }
-	public Task getTaskFromTrash(int index){ return trash.get(index); }
-	public ArrayList<Task> getPending(){ return pending; }
-	public ArrayList<Task> getComplete(){ return complete; }
-	public ArrayList<Task> getTrash(){ return trash; }
-	public ArrayList<Task> getSearchList() { return search; }
-	
+	public Task getTaskFromPending(int index) {
+		return pending.get(index);
+	}
+
+	public Task getTaskFromComplete(int index) {
+		return complete.get(index);
+	}
+
+	public Task getTaskFromTrash(int index) {
+		return trash.get(index);
+	}
+
+	public ArrayList<Task> getPendingList() {
+		return pending;
+	}
+
+	public ArrayList<Task> getCompleteList() {
+		return complete;
+	}
+
+	public ArrayList<Task> getTrashList() {
+		return trash;
+	}
+
+	public ArrayList<Task> getSearchList() {
+		return search;
+	}
+
 	// add new task functions
-	public void addTaskToPending(Task newPendingTask){ pending.add(newPendingTask); }
-	public void addTaskToComplete(Task newCompleteTask){ complete.add(newCompleteTask); }
-	public void addTaskToTrash(Task newTrashTask){ trash.add(newTrashTask); }
-   
-  // remove task functions return INVALID or VALID when remove a task
-  public int removeTaskFromPending(int index) {
-    if(index>pending.size()) return Control.INVALID; 
-     else {pending.remove(index);
-       return Control.VALID;}
-    }
-  public int removeTaskFromComplete (int index) {
-      if(index>pending.size()) return Control.INVALID; 
-     else {complete.remove(index);
-       return Control.VALID;}
-    }
-  public int removeTaskFromTrash (int index) {   
-      if(index>pending.size()) return Control.INVALID; 
-     else {trash.remove(index);
-       return Control.VALID;}
-    }
+	public void addTaskToPending(Task newPendingTask) {
+		pending.add(newPendingTask);
+	}
+
+	public void addTaskToComplete(Task newCompleteTask) {
+		complete.add(newCompleteTask);
+	}
+
+	public void addTaskToTrash(Task newTrashTask) {
+		trash.add(newTrashTask);
+	}
+
+	public void addTaskToSearch(Task newSearchTask) {
+		search.add(newSearchTask);
+	}
+
+	// remove task functions return INVALID or VALID when remove a task
+	public int removeTaskFromPending(int index) {
+		if (index > pending.size()) {
+			return Control.INVALID;
+		} else {
+			pending.remove(index);
+			return Control.VALID;
+		}
+	}
+
+	public int removeTaskFromComplete(int index) {
+		if (index > pending.size()) {
+			return Control.INVALID;
+		} else {
+			complete.remove(index);
+			return Control.VALID;
+		}
+	}
+
+	public int removeTaskFromTrash(int index) {
+		if (index > pending.size()) {
+			return Control.INVALID;
+		} else {
+			trash.remove(index);
+			return Control.VALID;
+		}
+	}
 }
--- src/Task.java
+++ src/Task.java
@@ -1,13 +1,14 @@
-class Task{
+class Task {
+
 	private boolean isImportant;
 	private CustomDate startDate;
 	private CustomDate endDate;
 	private String workInfo;
 	private String tag;
 	private int indexId;
-	
+
 	// default constructor
-	public Task(){
+	public Task() {
 		isImportant = false;
 		startDate = null;
 		endDate = null;
@@ -16,8 +17,8 @@ class Task{
 		indexId = 0;
 	}
 
-	public Task(boolean isImportant, CustomDate startDate, CustomDate endDate, 
-		String workInfo, String tag, int indexId){
+	public Task(boolean isImportant, CustomDate startDate, CustomDate endDate,
+			String workInfo, String tag, int indexId) {
 		this.isImportant = isImportant;
 		this.startDate = startDate;
 		this.endDate = endDate;
@@ -25,20 +26,54 @@ class Task{
 		this.tag = tag;
 		this.indexId = indexId;
 	}
-	
+
 	// get functions
-	public boolean getIsImportant(){ return isImportant; }	
-	public CustomDate getStartDate(){ return startDate; }	
-	public CustomDate getEndDate(){ return endDate; }	
-	public String getWorkInfo(){ return workInfo; }
-	public String getTag(){ return tag; }	
-	public int getIndexId(){ return indexId; }
-	
+	public boolean getIsImportant() {
+		return isImportant;
+	}
+
+	public CustomDate getStartDate() {
+		return startDate;
+	}
+
+	public CustomDate getEndDate() {
+		return endDate;
+	}
+
+	public String getWorkInfo() {
+		return workInfo;
+	}
+
+	public String getTag() {
+		return tag;
+	}
+
+	public int getIndexId() {
+		return indexId;
+	}
+
 	// set functions
-	public void setIsImportant(boolean isImportant){ this.isImportant = isImportant; }
-	public void setStartDate(CustomDate startDate){ this.startDate = startDate; }
-	public void setEndDate(CustomDate isImportant){ this.endDate = endDate; }
-	public void setWorkInfo(String workInfo){ this.workInfo = workInfo; }
-	public void setTag(String tag){ this.tag = tag; }
-	public void setIndexId(int indexId){ this.indexId = indexId; }
+	public void setIsImportant(boolean isImportant) {
+		this.isImportant = isImportant;
+	}
+
+	public void setStartDate(CustomDate startDate) {
+		this.startDate = startDate;
+	}
+
+	public void setEndDate(CustomDate endDate) {
+		this.endDate = endDate;
+	}
+
+	public void setWorkInfo(String workInfo) {
+		this.workInfo = workInfo;
+	}
+
+	public void setTag(String tag) {
+		this.tag = tag;
+	}
+
+	public void setIndexId(int indexId) {
+		this.indexId = indexId;
+	}
 }
