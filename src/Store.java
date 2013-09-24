dc0522f2ee2fc2e5d0b7d6270b7f3d321777a848
wangycbeyond <wangycbeyond@gmail.com>
2013-09-25 01:24:45 +0800

    Store classes merge with Wan Ling's new functions

---
+++ src/Store.java
@@ -0,0 +1,12 @@
+import java.io.IOException;
+
+public abstract class Store {
+	
+	public static void loadFromFile() throws IOException {
+		
+	};
+	
+	public static void storeToFile() throws IOException {
+		
+	};
+}
