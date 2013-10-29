b2d3d539d2b3de625f585aa364f3cc2e3f8d6e9d
pigoblock <pigoblock@localhost>
2013-10-29 22:53:50 +0800

    Changed save button

--- src/Login.java
+++ src/Login.java
@@ -131,8 +131,8 @@ public class Login {
 	private Button setupSaveButton(){
 		Button saveButton = new Button("");
 		saveButton.setId("save");
-		saveButton.setPrefSize(90, 42);
-		saveButton.setTranslateX(-120);
+		saveButton.setPrefSize(76, 42);
+		saveButton.setTranslateX(-95);
 		saveButton.setOnMouseClicked(new EventHandler<MouseEvent>() {
 			public void handle(MouseEvent e) {
 				if (storeUserInfo()){
