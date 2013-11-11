ad19a283d5eb1349faf4a5ef411cdfc91b06d2b0
wangycbeyond <wangycbeyond@gmail.com>
2013-11-11 18:11:48 +0800

    commit TaskStorageTest

---
+++ src/TaskStorageTest.java
@@ -0,0 +1,42 @@
+import static org.junit.Assert.*;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+//@author A0105667B
+public class TaskStorageTest {
+
+	static Control controlTest;
+	static Storage dataFile;
+
+	@BeforeClass
+	public static void testSetup() {
+		controlTest = new Control();
+		//Common.changeTaskFile("testTaskFile.xml");
+		controlTest.loadData();
+		dataFile = controlTest.getTaskFile();
+	}
+	
+	@Test
+	public void test() {
+		controlTest.executeCommand("add watch football game from 5pm to 7pm");
+		try {
+			dataFile.storeToFile();
+			assertTrue("Model are not stored successfully", dataFile.compareModelAndFileForTest());
+		} catch(Exception e) {
+			fail("Some exception thrown "+e.getMessage());
+		}
+		
+		controlTest.executeCommand("remove 3");
+		try {
+			dataFile.storeToFile();
+			assertTrue("Model are not stored successfully", dataFile.compareModelAndFileForTest());
+		} catch(Exception e) {
+			fail("Some exception thrown "+e.getMessage());
+		}
+		controlTest.executeCommand("undo");
+		controlTest.executeCommand("undo");
+	}
+	
+
+}
